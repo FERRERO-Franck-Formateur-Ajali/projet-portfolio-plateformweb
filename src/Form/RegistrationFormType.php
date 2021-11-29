63221f30bd8091902ba5366527be181e5955b8be
Marvinb67 <brassler.marvin@gmail.com>
2021-11-29 16:58:01 +0100

    register

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,10 +5,13 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
@@ -20,6 +23,14 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('jeSuis', ChoiceType::class, [
+                'label' => 'Je suis',
+                'mapped' => false,
+                'choices' => [
+                    'Particulier' => true,
+                    'Entrprise' => false,
+                ],
+            ])
             ->add('email', TextType::class, [
                 'constraints' => [
                     new Regex([
@@ -59,8 +70,17 @@ class RegistrationFormType extends AbstractType
             ])
             ->add('newsletter')
             ->add('compte', CompteType::class)
+            ;
 
-        ;
+        $func = function (FormEvent $event) {
+            $compte = $event->getData('Compte');
+            $formCompte = $event->getForm('Compte');
+            if ('jeSuis' === true) {
+                $formCompte->remove('raisonsocial');
+                dump($func);
+            }
+        };
+        $builder->addEventListener(FormEvents::POST_SET_DATA, $func);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
