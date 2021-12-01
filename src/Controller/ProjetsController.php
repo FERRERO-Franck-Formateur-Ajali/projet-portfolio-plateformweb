7e4267b290628b80ceb1825d3970ff38a6c455c1
Marvinb67 <brassler.marvin@gmail.com>
2021-12-01 16:59:32 +0100

    projets

--- src/Controller/ProjetsController.php
+++ src/Controller/ProjetsController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Projets;
+use App\Repository\ProjetsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +13,21 @@ class ProjetsController extends AbstractController
     /**
      * @Route("/projets", name="projets")
      */
-    public function index(): Response
+    public function index(ProjetsRepository $projetsRepository): Response
     {
         return $this->render('projets/index.html.twig', [
             'controller_name' => 'ProjetsController',
+            'projets' => $projetsRepository->findAll(9),
+        ]);
+    }
+
+    /**
+     * @Route("/projets/{slug}", name="projets_full")
+     */
+    public function full(Projets $projets): Response
+    {
+        return $this->render('projets/full.html.twig', [
+            'projets' => $projets,
         ]);
     }
 }
